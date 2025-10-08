fd49d068ba7f4414a466dd564c0d6e60399ac634
peterhuff <peterhuff@wustl.edu>
2025-10-08 11:32:12 -0400

    initialized, removed typescript, updated links and meta

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,5 @@
 import {
   isRouteErrorResponse,
-  Links,
-  Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -10,27 +8,13 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         {children}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <title>Sporkball</title>
+      <meta
+        name="description"
+        content="Sporkball Baseball homepage"
+      />
+      <p>This is the home page</p>
+    </div>
+  );
 }
