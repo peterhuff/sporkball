84b74600e88e85d5c5195ea03dc5367f253ea3b4
peterhuff <peterhuff@wustl.edu>
2025-12-10 15:56:12 -0500

    player card data and colors

--- app/routes/player.tsx
+++ app/routes/player.tsx
@@ -1,24 +1,74 @@
-import type { PlayerId, Player } from "~/util";
+import type { Player } from "~/util";
+import { Position, mlbTeams } from "~/util";
 import type { Route } from "./+types/player";
+
+enum Month {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+}
 
 export async function loader({ params }: Route.LoaderArgs) {
     const { playerId } = params;
 
-    const url = `https://statsapi.mlb.com/api/v1/people/${playerId}?fields=people,id,fullName,firstName,lastName`;
+    const url = `https://statsapi.mlb.com/api/v1/people/${playerId}?hydrate=currentTeam,rosterEntries&fields=people,id,fullName,birthDate,birthCity,birthStateProvince,birthCountry,height,weight,rosterEntries,currentTeam,name,parentOrgId,primaryPosition,code,mlbDebutDate,batSide,description,pitchHand,fullFMLName`;
     const response = await fetch(url)
         .then((res) => res.json());
-    const player: PlayerId = response.people[0];
+    const rawPlayer = response.people[0];
 
-    // const getCurrentOrg = async (team: { name: string, parentOrgId?: number }) => {
-    //     if (team.parentOrgId) {
-    //         const orgUrl = `https://statsapi.mlb.com/api/v1/teams/${team.parentOrgId}`;
-    //         const orgTeam = await fetch(orgUrl)
-    //             .then((res) => res.json());
-    //         return orgTeam.teams[0].name;
-    //     }
-    //     return player.currentTeam.name;
-    // };
-    // const currentOrg = await getCurrentOrg(player.currentTeam);
+    const player: Player = {
+        fullName: rawPlayer.fullName,
+        id: rawPlayer.id,
+        fullFMLName: rawPlayer.fullFMLName,
+        birthInfo: {
+            date: rawPlayer.birthDate,
+            city: rawPlayer.birthCity,
+            stateProvince: rawPlayer.birthStateProvince, // ?? undefined
+            country: rawPlayer.birthCountry,
+        },
+        height: rawPlayer.height,
+        weight: rawPlayer.weight,
+        isActive: rawPlayer.rosterEntries[0].isActive,
+        primaryPosition: codeToPosition(rawPlayer.primaryPosition.code),
+        debutDate: rawPlayer.mlbDebutDate,
+        batSide: rawPlayer.batSide.description,
+        throwHand: rawPlayer.pitchHand.description,
+    };
+
+    for (const entry of rawPlayer.rosterEntries) {
+        if (entry.team.parentOrgId &&
+            ((entry.team.parentOrgId >= 133 && entry.team.parentOrgId <= 147)
+                || (entry.team.parentOrgId == 158)
+                || (entry.team.parentOrgId >= 108 && entry.team.parentOrgId <= 121))
+        ) {
+            if (entry.isActive) {
+                player.currentOrg = {
+                    id: entry.team.parentOrgId,
+                    name: mlbTeams[entry.team.parentOrgId as keyof typeof mlbTeams].name,
+                }
+            }
+            break;
+        }
+        if ((entry.team.id >= 133 && entry.team.id <= 147) || (entry.team.id == 158) || (entry.team.parentOrgId >= 108 && entry.team.parentOrgId <= 121)) {
+            if (entry.isActive) {
+                player.currentOrg = {
+                    id: entry.team.id,
+                    name: entry.team.name,
+                };
+            }
+            break;
+        }
+    }
+
     return { player };
 }
 
@@ -29,44 +79,74 @@ export default function Player({ loaderData }: Route.ComponentProps) {
     return (
         <div className="player-page">
             <title>{`${player.fullName} | Sporkball`}</title>
-            <div className="bio-card">
+            <div 
+                className="bio-card" 
+                style={{ backgroundColor: player.currentOrg ? mlbTeams[player.currentOrg.id].color : "white" }}>
                 <div className="name-team">
                     <h1>{player.fullName}</h1>
-                    <h2>San Fransisco Giants</h2>
+                    {player.currentOrg ?
+                        <h2>{player.currentOrg.name}</h2>
+                        : null
+                    }
                 </div>
-                
-                <img
-                    className="player-icon"
-                    src={`https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_400,q_auto:best/v1/people/${player.id}/headshot/67/current`}
-                />
                 <div className="player-info">
+                    <img
+                        className="player-icon"
+                        src={`https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_400,q_auto:best/v1/people/${player.id}/headshot/67/current`}
+                    />
                     <div className="info-line">
-                        <p className="info-header">Position:</p>
+                        <p className="info-header">{`Position: ${Position[player.primaryPosition]}`}</p>
                     </div>
                     <div className="info-line">
-                        <p className="info-header">Bats:</p>
+                        <p className="info-header">{`Bats: ${player.batSide}`}</p>
                         <p>•</p>
-                        <p className="info-header">Throws:</p>
+                        <p className="info-header">{`Throws: ${player.throwHand}`}</p>
                     </div>
                     <div className="info-line">
-                        <p className="info-header">Height:</p>
+                        <p className="info-header">{`Height: ${player.height}`}</p>
                         <p>•</p>
-                        <p className="info-header">Weight:</p>
+                        <p className="info-header">{`Weight: ${player.weight}`}</p>
                     </div>
                     <div className="info-line">
-                        <p className="info-header">Full Name:</p>
+                        <p className="info-header">{`MLB Debut: ${parseDate(player.debutDate)}`}</p>
                     </div>
                     <div className="info-line">
-                        <p className="info-header">Birthdate:</p>
+                        <p className="info-header">{`Birthdate: ${parseDate(player.birthInfo.date)}`}</p>
                     </div>
                     <div className="info-line">
-                        <p className="info-header">Birthplace:</p>
+                        <p className="info-header">
+                            {"Birthplace: " + player.birthInfo.city + ", "}
+                            {player.birthInfo.stateProvince ? player.birthInfo.stateProvince + ", " : null}
+                            {player.birthInfo.country}
+                        </p>
                     </div>
                     <div className="info-line">
-                        <p className="info-header">MLB Debut:</p>
+                        <p className="info-header">{`Full Name: ${player.fullFMLName}`}</p>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
+
+const codeToPosition = (code: number | 'O' | 'Y') => {
+    switch (code) {
+        case 'O':
+            return 11;
+        case 'Y':
+            return 12;
+        default:
+            return code;
+    }
+};
+
+const parseDate = (date: string) => {
+    const splitDate = date.split('-');
+    if (splitDate[2].startsWith('0')) {
+        splitDate[2] = splitDate[2].slice(1);
+    }
+    const newDate = `${Month[+splitDate[1]]} ${splitDate[2]}, ${splitDate[0]}`
+    return newDate;
+};
+
+
--- app/util.ts
+++ app/util.ts
@@ -14,17 +14,186 @@ export interface Player extends PlayerId {
     }
     height: string;
     weight: number;
-    active: boolean;
-    currentTeam: {
-        id: number;
+    isActive: boolean;
+    currentOrg?: {
+        id: TeamCode;
         name: string;
-        parentOrgId?: number;
     }
-    primaryPosition: string;
+    primaryPosition: number;
     debutDate: string;
     batSide: string;
     throwHand: string;
 }
+
+export enum Position {
+    Pitcher = 1,
+    Catcher,
+    "First Baseman",
+    "Second Baseman",
+    "Third Baseman",
+    Shortstop,
+    Leftfielder,
+    Centerfielder,
+    Rightfielder,
+    "Designated Hitter",
+    Outfielder,
+    "Two-Way",
+}
+
+type TeamCode = keyof typeof mlbTeams;
+
+export const mlbTeams = {
+    133: {
+        name: "Athletics",
+        abbreviation: "ATH",
+        color: "#EFB21E",
+    },
+    134: {
+        name: "Pittsburgh Pirates",
+        abbreviation: "PIT",
+        color: "#FDB827",
+    },
+    135: {
+        name: "San Diego Padres",
+        abbreviation: "SDP",
+        color: "#FFC425",
+    },
+    136: {
+        name: "Seattle Mariners",
+        abbreviation: "SEA",
+        color: "#005C5C",
+    },
+    137: {
+        name: "San Fransisco Giants",
+        abbreviation: "SFG",
+        color: "#FD5A1E",
+    },
+    138: {
+        name: "St. Louis Cardinals",
+        abbreviation: "STL",
+        color: "#C41E3A",
+    },
+    139: {
+        name: "Tampa Bay Rays",
+        abbreviation: "TBR",
+        color: "#8FBCE6",
+    },
+    140: {
+        name: "Texas Rangers",
+        abbreviation: "TEX",
+        color: "#003278",
+    },
+    141: {
+        name: "Toronto Blue Jays",
+        abbreviation: "TOR",
+        color: "#134A8E",
+    },
+    142: {
+        name: "Minnesota Twins",
+        abbreviation: "MIN",
+        color: "#D31145",
+    },
+    143: {
+        name: "Philadelphia Phillies",
+        abbreviation: "PHI",
+        color: "#E81828",
+    },
+    144: {
+        name: "Atlanta Braves",
+        abbreviation: "ATL",
+        color: "#CE1141",
+    },
+    145: {
+        name: "Chicago White Sox",
+        abbreviation: "CHW",
+        color: "#C4CED4",
+    },
+    146: {
+        name: "Miami Marlins",
+        abbreviation: "MIA",
+        color: "#00A3E0",
+    },
+    147: {
+        name: "New York Yankees",
+        abbreviation: "NYY",
+        color: "#C4CED3",
+    },
+    158: {
+        name: "Milwaukee Brewers",
+        abbreviation: "MIL",
+        color: "#FFC52F",
+    },
+    108: {
+        name: "Los Angeles Angels",
+        abbreviation: "LAA",
+        color: "#BA0021",
+    },
+    109: {
+        name: "Arizona Diamondbacks",
+        abbreviation: "ARI",
+        color: "#A71930",
+    },
+    110: {
+        name: "Baltimore Orioles",
+        abbreviation: "BAL",
+        color: "#DF4601",
+    },
+    111: {
+        name: "Boston Red Sox",
+        abbreviation: "BOS",
+        color: "#BD3039",
+    },
+    112: {
+        name: "Chicago Cubs",
+        abbreviation: "CHC",
+        color: "#0E3386",
+    },
+    113: {
+        name: "Cincinnati Reds",
+        abbreviation: "CIN",
+        color: "#C6011F",
+    },
+    114: {
+        name: "Cleveland Guardians",
+        abbreviation: "CLE",
+        color: "#00385D",
+    },
+    115: {
+        name: "Colorado Rockies",
+        abbreviation: "COL",
+        color: "#333366",
+    },
+    116: {
+        name: "Detroit Tigers",
+        abbreviation: "Det",
+        color: "#FA4616",
+    },
+    117: {
+        name: "Houston Astros",
+        abbreviation: "HOU",
+        color: "#EB6E1F",
+    },
+    118: {
+        name: "Kansas City Royals",
+        abbreviation: "KCR",
+        color: "#004687",
+    },
+    119: {
+        name: "Los Angeles Dodgers",
+        abbreviation: "LAD",
+        color: "#005A9C",
+    },
+    120: {
+        name: "Washington Nationals",
+        abbreviation: "WAS",
+        color: "#AB0003",
+    },
+    121: {
+        name: "New York Mets",
+        abbreviation: "NYM",
+        color: "#002D72",
+    },
+};
 
 // export async function getTop(): Promise<Array<PlayerId>> {
 //     const url = "https://statsapi.mlb.com/api/v1/stats/leaders?leaderCategories=totalPlateAppearances&fields=leagueLeaders,leaders,person,id,firstName,lastName&limit=10";
